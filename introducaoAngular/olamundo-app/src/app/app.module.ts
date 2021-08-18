438aba2dd995acd59a70d23494131ccfe2f0c352
Edmilson <scovinowork@gmail.com>
2021-08-18 16:01:29 -0300

    Colocando componentes  no rodapé

--- introducaoAngular/olamundo-app/src/app/app.module.ts
+++ introducaoAngular/olamundo-app/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     RodapeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TopoComponent, RodapeComponent]
 })
 export class AppModule { }

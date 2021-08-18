61c663951ca2a02bd3745cfb71e2e3d88dc81b7c
Edmilson <scovinowork@gmail.com>
2021-08-18 16:42:52 -0300

    toolbar

--- introducaoAngular/olamundo-app/src/app/app.module.ts
+++ introducaoAngular/olamundo-app/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -16,7 +19,9 @@ import { RodapeComponent } from './rodape/rodape.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
 
 
   ],

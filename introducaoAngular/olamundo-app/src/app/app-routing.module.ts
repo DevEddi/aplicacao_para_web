63fb309c37b23704d96794c9c2bf0124c6755300
Edmilson <scovinowork@gmail.com>
2021-08-25 12:55:12 -0300

    Fazendo alguns testes com transição de componentes

--- introducaoAngular/olamundo-app/src/app/app-routing.module.ts
+++ introducaoAngular/olamundo-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PrincipalComponent} from "./principal/principal.component";
+import {ContatosComponent} from "./contatos/contatos.component";
 
 const routes: Routes = [
   {path: '', component: PrincipalComponent},
+  {path: 'contatos', component: ContatosComponent},
 ];
 
 @NgModule({
